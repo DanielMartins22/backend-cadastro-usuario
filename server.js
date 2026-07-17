d14b14c77887945b2db7a319d84b50f3732d525e
DanielMartins22 <danielmn.contato@hotmail.com>
2026-07-17 18:29:22 -0300

    ajustes para Railway

--- server.js
+++ server.js
@@ -75,7 +75,11 @@ app.delete('/usuarios/:id', async (request, response) => {
     }
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 
 // request/rq = requisiçao
 // response/re = resposta
